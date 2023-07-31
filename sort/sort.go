7572e1a1743d8d46ef9665a5eda03a6e111d8bb8
Kamimura Teppei <teppei_kamimura@pkshatech.com>
2023-07-31 15:52:56 +0900

    sort/sort.go を修正しました。
    降順にソートして返すようにしました。
    これは、コサイン類似度を優先度の高い順に返すことを想定しています。

--- sort/sort.go
+++ sort/sort.go
@@ -1,19 +1,12 @@
 package sort
 
 import (
-	"fmt"
 	"sort"
 )
 
-func Sort(s1 []float64) []float64{
-
-	// 昇順
-	sort.Sort(sort.Float64Slice(s1))
-	fmt.Printf("len:%d cap:%d v:%v\n", len(s1), cap(s1), s1)
+func RevSort(s1 []float64) []float64{
 
 	// 降順
 	sort.Sort(sort.Reverse(sort.Float64Slice(s1)))
-	fmt.Printf("len:%d cap:%d v:%v\n", len(s1), cap(s1), s1)
-
 	return s1
 }
