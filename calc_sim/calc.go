0b102c9c0173fc86b70cd7931a0663dec743a5d3
Kamimura Teppei <teppei_kamimura@pkshatech.com>
2023-07-31 16:14:39 +0900

    calc_sim/calc.go を修正しました。
    受け取ったベクトルを規格化してから内積を取るようにしました。

--- calc_sim/calc.go
+++ calc_sim/calc.go
@@ -1,7 +1,14 @@
 package calc_sim
 
+import(
+	"github.com/ASAP-BYAF/cos_similarity/calc_sim/stdize_vec"
+)
 func Calc(x, y []float64) float64 {
 
+	// 規格化
+	stdize_vec.Stdize(x)
+	stdize_vec.Stdize(y)
+  
 	var similarity float64 = 0.0
 	for i := 0; i < len(x); i++ {
 		similarity += x[i] * y[i]
