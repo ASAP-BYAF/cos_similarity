634509b8a1a09aa288ffb413462b9da2624045fa
Kamimura Teppei <teppei_kamimura@pkshatech.com>
2023-07-31 17:05:59 +0900

    main.go を修正しました。
    ベクトルのリストを作成し、その各要素と特定の１つのベクトルとの間の類似度を計算するようにしました。
    また、それぞれの値をスライスに格納し、値の大きい順にソートして出力するようにしました。

--- main.go
+++ main.go
@@ -4,21 +4,29 @@ import(
   "fmt"
 
   "github.com/ASAP-BYAF/cos_similarity/calc_sim"
-  "github.com/ASAP-BYAF/cos_similarity/calc_sim/stdize_vec"
   "github.com/ASAP-BYAF/cos_similarity/sort"
 )
 
 func main() {
-  s1 := []float64{0.3, 0.4, 0.5, 0.9, 1.0}
-  s2 := []float64{0.4, 0.1, 0.6, 1.0, 1.0}
-  sim := calc_sim.Calc(s1, s2)
+  // s1 を質問分をベクトルしたものとする。
+  s1 := []float64{0.3, 0.4, 0.5}
+  s2 := []float64{0.4, 0.1, 0.6}
+  s3 := []float64{0.2, 0.1, 0.5}
+  s4 := []float64{1.3, 3.5, 2.5}
+  s_list := [][]float64{s2, s3, s4}
 
-  fmt.Printf("sim = %g\n", sim)
+  // ファイルのベクトルのそれぞれに対して質問文とのコサイン類似度を計算
+  // sim_list に格納。
+  var sim float64 = 0.0
+  sim_list := []float64{}
+  for i := 0; i < len(s_list); i++ {
+    sim = calc_sim.Calc(s1, s_list[i])
+    sim_list = append(sim_list, sim)
+	}
 
-  sort.RevSort(s1)
-  fmt.Printf("s1_sorted = %v\n", s1)
+  // 優先度の高い順にソート
+  sort.RevSort(sim_list)
 
-  fmt.Printf("s1_before_stdize = %v\n", s1)
-  stdize_vec.Stdize(s1)
-  fmt.Printf("s1_after_stdize = %v\n", s1)
+  // 確認
+  fmt.Printf("sim_list_sorted = %v\n", sim_list)
 }
